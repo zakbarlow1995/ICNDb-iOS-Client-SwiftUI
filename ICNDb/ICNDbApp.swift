de3eb9deee14ec941b743932149100328349f5d8
zakbarlow1995 <zak@stormideas.com>
2021-09-21 17:49:33 +0100

    Initial Commit

---
+++ ICNDb/ICNDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICNDbApp.swift
+//  ICNDb
+//
+//  Created by Zak Barlow on 21/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ICNDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
